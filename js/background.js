00528fb02038ab9791ae9a38ab0a533c0a537de7
EZForever <34133756+EZForever@users.noreply.github.com>
2023-04-08 12:40:20 +0800

    右键菜单的一系列改进

--- js/background.js
+++ js/background.js
@@ -2,14 +2,46 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create(
       {
         type: 'normal',
-        title: chrome.i18n.getMessage("sendTo") + '“%s”',
-        id: 'Memos-send',
-        contexts: ['all']
+        title: chrome.i18n.getMessage("sendTo"),
+        id: 'Memos-send-selection',
+        contexts: ['selection']
+      },
+    )
+    chrome.contextMenus.create(
+      {
+        type: 'normal',
+        title: chrome.i18n.getMessage("sendLinkTo"),
+        id: 'Memos-send-link',
+        contexts: ['link', 'page']
+      },
+    )
+    chrome.contextMenus.create(
+      {
+        type: 'normal',
+        title: chrome.i18n.getMessage("sendImageTo"),
+        id: 'Memos-send-image',
+        contexts: ['image']
       },
     )
 })
-let tempCont=''
 chrome.contextMenus.onClicked.addListener(info => {
-    tempCont += info.selectionText + '\n'
-    chrome.storage.sync.set({open_action: "save_text",open_content:tempCont});
+    let tempCont=''
+    switch(info.menuItemId){
+      case 'Memos-send-selection':
+        tempCont = info.selectionText + '\n'
+        break
+      case 'Memos-send-link':
+        tempCont = (info.linkUrl || info.pageUrl) + '\n'
+        break
+      case 'Memos-send-image':
+        tempCont = `![](${info.srcUrl})` + '\n'
+        break
+    }
+    chrome.storage.sync.get({open_action: "save_text", open_content: ''}, function(items) {
+      if(items.open_action === 'upload_image') {
+        alert(chrome.i18n.getMessage("picPending"));
+      } else {
+        chrome.storage.sync.set({open_action: "save_text", open_content: items.open_content + tempCont});
+      }
+    })
 })
